69c8c2ae11c3e60909659c733c56152494ebe916
Marcosscarpini <134976567+Marcosscarpini@users.noreply.github.com>
2023-09-18 19:24:10 -0300

    kgfdikglg

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ function tocaSomClap () {
 }
 
 document.querySelector ('.tecla_clap').onclick = tocaSomClap
+
+function tocaSomTim () {
+    document.querySelector ('#som_tecla_tim').play();
+}
+
+document.querySelector ('.tecla_tim').onclick = tocaSomTim
+
+function tocaSomPuff () {
+    document.querySelector ('#som_tecla_puff').play();
+}
+
+document.querySelector ('.tecla_puff').onclick = tocaSomPuff
+
+function tocaSomSplash () {
+    document.querySelector ('#som_tecla_splash').play();
+}
+
+document.querySelector ('.tecla_splash').onclick = tocaSomSplash
